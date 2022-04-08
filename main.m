8c0d33465c1bb85a1249574c63b87c2a05bd76bb
Matthew Knowles <mattknowles314@gmail.com>
2022-04-08 14:21:15 +0100

    IT WORKS

--- main.m
+++ main.m
@@ -8,13 +8,15 @@ T = 0.5; %Time to maturity (years)
 
 %Simulation Data
 Nmc = 100; %Number of simulations
-M = 160; %Number of time steps
+M = 140; %Number of time steps
 N = 150; %Number of space steps
 
 [MCPrice, MCError] = monte_carlo(S0,K,B,r,q,T,Nmc,M);
 EXPPrice = explicit(S0,K,B,r,q,T,N,M);
 IMPPrice = implicit(S0,K,B,r,q,T,N,M);
+CRNPrice = crank(S0,K,B,r,q,T,N,M);
 
 MCPrice
 EXPPrice
 IMPPrice
+CRNPrice
